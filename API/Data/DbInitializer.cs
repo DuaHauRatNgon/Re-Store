c9c0dd3c832fd5564016fc345aefd865c94a2023
Dua Hau Rat Ngon <nmhuongbn@gmail.com>
2023-09-28 15:27:44 +0700

    Init API + React

---
+++ API/Data/DbInitializer.cs
@@ -0,0 +1,215 @@
+﻿using API.Entities;
+
+namespace API.Data {
+    public static class DbInitializer {
+        public static void Initialize(StoreContext context) {
+            if (context.Products.Any()) return;
+
+            var products = new List<Product>
+            {
+                new Product
+                {
+                    Name = "Khăn Choàng Cổ Nữ Cashmere ",
+                    Description =
+                        "️Sản phẩm được làm từ chất liệu vải len mịn (không phải len xù), thân thiện với người dùng, không gây kích ứng da.",
+                    Price = 20000,
+                    PictureUrl = "/images/products/khan1.png",
+                    Brand = "Hương Lan",
+                    Type = "Khăn quàng",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Khăn len loại 2",
+                    Description = "Nunc viverra imperdiet enim. Fusce est. Vivamus a tellus.",
+                    Price = 15000,
+                    PictureUrl = "/images/products/sb-ang2.png",
+                    Brand = "Angular",
+                    Type = "Boards",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Khăn len loại 3",
+                    Description =
+                        "Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc. Mauris eget neque at sem venenatis eleifend. Ut nonummy.",
+                    Price = 18000,
+                    PictureUrl = "/images/products/sb-core1.png",
+                    Brand = "NetCore",
+                    Type = "Boards",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Khăn len loại 4",
+                    Description =
+                        "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin pharetra nonummy pede. Mauris et orci.",
+                    Price = 30000,
+                    PictureUrl = "/images/products/sb-core2.png",
+                    Brand = "NetCore",
+                    Type = "Boards",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Khăn len loại 5",
+                    Description =
+                        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.",
+                    Price = 25000,
+                    PictureUrl = "/images/products/sb-react1.png",
+                    Brand = "React",
+                    Type = "Boards",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Khăn len loại 6",
+                    Description =
+                        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.",
+                    Price = 12000,
+                    PictureUrl = "/images/products/sb-ts1.png",
+                    Brand = "TypeScript",
+                    Type = "Boards",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Mũ len 1",
+                    Description =
+                        "Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.",
+                    Price = 1000,
+                    PictureUrl = "/images/products/hat-core1.png",
+                    Brand = "NetCore",
+                    Type = "Hats",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Mũ len 2",
+                    Description =
+                        "Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.",
+                    Price = 8000,
+                    PictureUrl = "/images/products/hat-react1.png",
+                    Brand = "React",
+                    Type = "Hats",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Mũ len 3",
+                    Description =
+                        "Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.",
+                    Price = 1500,
+                    PictureUrl = "/images/products/hat-react2.png",
+                    Brand = "React",
+                    Type = "Hats",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Găng tay 1",
+                    Description =
+                        "Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.",
+                    Price = 1800,
+                    PictureUrl = "/images/products/glove-code1.png",
+                    Brand = "VS Code",
+                    Type = "Gloves",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Găng tay 2",
+                    Description =
+                        "Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.",
+                    Price = 1500,
+                    PictureUrl = "/images/products/glove-code2.png",
+                    Brand = "VS Code",
+                    Type = "Gloves",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Găng tay 3",
+                    Description =
+                        "Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.",
+                    Price = 1600,
+                    PictureUrl = "/images/products/glove-react1.png",
+                    Brand = "React",
+                    Type = "Gloves",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Găng tay 4",
+                    Description =
+                        "Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.",
+                    Price = 1400,
+                    PictureUrl = "/images/products/glove-react2.png",
+                    Brand = "React",
+                    Type = "Gloves",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Giày 1",
+                    Description =
+                        "Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc. Mauris eget neque at sem venenatis eleifend. Ut nonummy.",
+                    Price = 25000,
+                    PictureUrl = "/images/products/boot-redis1.png",
+                    Brand = "Redis",
+                    Type = "Boots",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Giày 2",
+                    Description =
+                        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas porttitor congue massa. Fusce posuere, magna sed pulvinar ultricies, purus lectus malesuada libero, sit amet commodo magna eros quis urna.",
+                    Price = 18999,
+                    PictureUrl = "/images/products/boot-core2.png",
+                    Brand = "NetCore",
+                    Type = "Boots",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Giày 3",
+                    Description =
+                        "Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin pharetra nonummy pede. Mauris et orci.",
+                    Price = 19999,
+                    PictureUrl = "/images/products/boot-core1.png",
+                    Brand = "NetCore",
+                    Type = "Boots",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Giày 4",
+                    Description = "Aenean nec lorem. In porttitor. Donec laoreet nonummy augue.",
+                    Price = 15000,
+                    PictureUrl = "/images/products/boot-ang2.png",
+                    Brand = "Angular",
+                    Type = "Boots",
+                    QuantityInStock = 100
+                },
+                new Product
+                {
+                    Name = "Giày 5",
+                    Description =
+                        "Suspendisse dui purus, scelerisque at, vulputate vitae, pretium mattis, nunc. Mauris eget neque at sem venenatis eleifend. Ut nonummy.",
+                    Price = 18000,
+                    PictureUrl = "/images/products/boot-ang1.png",
+                    Brand = "Angular",
+                    Type = "Boots",
+                    QuantityInStock = 100
+                },
+            };
+
+            foreach (var product in products) {
+                context.Products.Add(product);
+            }
+
+            context.SaveChanges();
+        }
+    }
+}
---
+++ Client/src/app/layout/App.tsx
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+import '../../App.css'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { Container, CssBaseline, ThemeProvider, createTheme } from '@mui/material';
+import Header from './Header';
+import Catalog from '../../features/Catalog';
+
+
+function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const palleteType = darkMode ? 'dark' : 'light';
+  const theme = createTheme({
+    palette: {
+      mode: palleteType,
+      background: {
+        default: (palleteType === 'light') ? '#eaeaea' : '#121212'
+      }
+    }
+  })
+
+  function handleThemeChange() {
+    setDarkMode(!darkMode);
+  }
+  
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Header darkMode={darkMode} handleThemeChange={handleThemeChange}  />
+      <Container>
+        <Catalog />
+      </Container>
+    </ThemeProvider>
+  );
+}
+
+export default App;
